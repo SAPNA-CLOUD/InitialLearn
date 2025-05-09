7c8953803bd57d065b99ebca2721cf4f383dadd3
SAPNA-CLOUD <85059831+SAPNA-CLOUD@users.noreply.github.com>
2025-05-09 12:00:12 +0530

    Update About.js

--- src/components/Portfolio/About.js
+++ src/components/Portfolio/About.js
@@ -21,24 +21,6 @@ const About = () => {
         <p>The device creates a series of tones and lights and requires a user to repeat the sequence.  If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over. </p>
         </div>
         
-        <div className='pleft'>
-        <img src={Simon_Game} alt="E-Commerce Website">
-        </img> 
-        </div>
-        <div className='pright'>
-        <a href='https://ecommercesapna.vercel.app/' className='simon'><h3>Simon Game</h3></a> <br/>
-        <p>This website is made to purchase clothes for men, women and kids. </p>
-        </div>
-
-        <div className='pleft'>
-        <img src={Simon_Game} alt="Mobile Website">
-        </img> 
-        </div>
-        <div className='pright'>
-        <a href='https://argusacademy.com/sapna/' className='simon'><h3>Simon Game</h3></a> <br/>
-        <p>Mobile Website is used to sell and purchase old mobile phones. </p>
-        </div>
-        
         
       </div>
     <button className='portfolioBtn'>See More</button>
